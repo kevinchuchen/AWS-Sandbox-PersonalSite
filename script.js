290c57058a5d77a9e740ea420f20b42e9c3e7d5c
kevinchuchen <59314509+kevinchuchen@users.noreply.github.com>
2023-01-03 12:25:42 +0300

    modifies TODO

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //TODO :	DONE--rewrite focus on navbutton
+//			clean up section 1 Navbar HTML code (reduce duplicates)
 //			Add background on section 1
 //			change nav-icon color to white
 $(function() {
