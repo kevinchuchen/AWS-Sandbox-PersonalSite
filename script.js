9a2be37972391a2fb14a9c9c49271d5b867bb296
kevinchuchen <59314509+kevinchuchen@users.noreply.github.com>
2023-01-03 17:26:56 +0300

    TODO-Changes structure to allow for transparent bottom arrow at the bottom of page

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 //			clean up section 1 Navbar HTML code (reduce duplicates)
 //			Add background on section 1
 //			change nav-icon color to white
+//			try linear gradient for section 2 bottom-arrow
 $(function() {
 	$.scrollify({
 		section: "section",
