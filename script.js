2defed2c741458381e2df0dcff6796e8ff72a64c
kevinchuchen <59314509+kevinchuchen@users.noreply.github.com>
2023-01-05 09:41:04 +0300

    modifies scrollify options to make it work with touch

--- script.js
+++ script.js
@@ -3,10 +3,20 @@
 //			Add background on section 1
 //			change nav-icon color to white
 //			Set hyperlink
-
+//			Brand-logo wraps on small device, try and change responsive font size
+//			make it work on touchpad
 $(function() {
 	$.scrollify({
 		section: "section",
-		scrollSpeed:1,
+		scrollSpeed:500,
+		touchScroll:true,
+		before:function(i,panels){
+			$.scrollify.update()
+			console.log(i);
+			console.log("test");
+			console.log(panels);
+			
+		}
 	});
 });
+
