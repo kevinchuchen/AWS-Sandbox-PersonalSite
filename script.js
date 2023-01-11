fc59b931a9c49f02317cedca32fbb15ec034296a
kevinchuchen <59314509+kevinchuchen@users.noreply.github.com>
2023-01-11 16:19:54 +0300

    Bug fixes

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 //TODO :	DONE--rewrite focus on navbutton
-//			clean up section 1 Navbar HTML code (reduce duplicates)
+//			DONE--clean up section 1 Navbar HTML code (reduce duplicates)
 //			Add background on section 1
 //			change nav-icon color to white
-//			Set hyperlink
-new fullpage('#fullpage', {
-	//options here
-	autoScrolling:true,
-	scrollHorizontally: true,
-	credits:{ enabled: true, label: 'test2 with fullPage.js', position: 'right'},
-	verticalCentered: false,
+//			DONE--Set hyperlink and navbar redirection
+//			Brand-logo wraps on small device, try and change responsive font size
+//			DONE--make scrolling work on touchpad
+$(function() {
+	$.scrollify({
+		section: "section",
+		scrollSpeed:1400,
+		easing: "easeOutExpo",
+		touchScroll:true,
+		scrollbars:true,
+		afterRender:function(i, panels){
+		$.scrollify.update()
+		$(".pagination a").on("click",$.scrollify.move);
+		}
+	});
 });
+
+
+
