8a457d840ba5d4e79b94f1b6a09d1adcfa800f8b
kevinchuchen <59314509+kevinchuchen@users.noreply.github.com>
2023-01-11 15:58:45 +0300

    Misc update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //TODO :	DONE--rewrite focus on navbutton
-//			clean up section 1 Navbar HTML code (reduce duplicates)
+//			DONE--clean up section 1 Navbar HTML code (reduce duplicates)
 //			Add background on section 1
 //			change nav-icon color to white
-//			Set hyperlink
+//			DONE--Set hyperlink and navbar redirection
 //			Brand-logo wraps on small device, try and change responsive font size
-//			make it work on touchpad
+//			DONE--make scrolling work on touchpad
 $(function() {
 	$.scrollify({
 		section: "section",
@@ -13,12 +13,11 @@ $(function() {
 		touchScroll:true,
 		scrollbars:true,
 		afterRender:function(i, panels){
-			$.scrollify.update()
+		$.scrollify.update()
+		$(".pagination a").on("click",$.scrollify.move);
 		}
 	});
 });
 
-/*
-$(".pagination a").on("click",$.scrollify.move());
 
-*/
+
