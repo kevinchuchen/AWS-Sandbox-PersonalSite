7488151b8f1af7e378e455bb7c1c35f02f35217e
Lim Chen <kevinchuchen@gmail.com>
2023-01-18 17:26:50 +0800

    Disables touch-scroll and expand on TODO

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 //TODO :	DONE--rewrite focus on navbutton
 //			DONE--clean up section 1 Navbar HTML code (reduce duplicates)
+//			Add social links (github, linkedin, personal site)
+//			View counter via javascript
+//			API calls to Dynamo DB
 //			Add background on section 1
 //			change nav-icon color to white
 //			DONE--Set hyperlink and navbar redirection
-//			Brand-logo wraps on small device, try and change responsive font size
+//			DONE--Brand-logo wraps on small device, try and change responsive font size
 //			DONE--make scrolling work on touchpad
 $(function() {
 	$.scrollify({
 		section: "section",
 		scrollSpeed:1400,
 		easing: "easeOutExpo",
-		touchScroll:true,
+		touchScroll:false,
 		scrollbars:true,
 		afterRender:function(i, panels){
 		$.scrollify.update()
