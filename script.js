e5775455065e45ea76d5ee34e501bab458c42924
Lim Chen <kevinchuchen@gmail.com>
2023-01-22 18:44:14 +0800

    adds viewcount support

--- script.js
+++ script.js
@@ -22,5 +22,22 @@ $(function() {
 	});
 });
 
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
 
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
 
+var client = new HttpClient();
+client.get('https://5qrbpd5xb9.execute-api.us-east-1.amazonaws.com/default/incrementViewCount', function(response) {
+    // do something with response
+	alert(response);
+	console.log(response);
+});
