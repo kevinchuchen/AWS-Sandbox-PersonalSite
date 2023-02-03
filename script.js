6cdde182746c11198cfec779eb49edd2a90dc4e0
Lim Chen <kevinchuchen@gmail.com>
2023-02-04 00:13:11 +0800

    Bug Fixes, Background change, Formatting change

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 //			DONE--Add social links (github, linkedin, personal site)
 //			DONE--View counter via javascript
 //			DONE--API calls to Dynamo DB
-//			Add background on section 1
+//			DONE--Add background on section 1
+//			Remove touchscroll completely
 //			Add animations to make it more interactive
 //			Add error.html
-//			Modify discription of architectural roadmap
-//			change nav-icon color to white
+//			DONE--Modify discription of architectural roadmap
 //			DONE--Set hyperlink and navbar redirection
 //			DONE--Brand-logo wraps on small device, try and change responsive font size
 //			DONE--make scrolling work on touchpad
@@ -38,11 +38,11 @@ var HttpClient = function() {
     }
 }
 
-// var client = new HttpClient();
-// client.get('https://api.kevinlim.cc/incrementViewCount',function(response) {
-//     // update page view with custom API call
-// 	pageView = "Page views: " + String(response); 
-// 	document.getElementById("pageview").textContent= pageView;
-// 	console.log(response);
-// });
+var client = new HttpClient();
+client.get('https://api.kevinlim.cc/incrementViewCount',function(response) {
+    // update page view with custom API call
+	pageView = "Page views: " + String(response); 
+	document.getElementById("pageview").textContent= pageView;
+	console.log(response);
+});
 
